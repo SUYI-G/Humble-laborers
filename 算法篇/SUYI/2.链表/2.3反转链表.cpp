ab1a4d6fcc41d77274687cafd146fa438ec6c3cc
SUYI-G <13052209268@163.com>
2021-10-06 06:24:26 +0800

    10.5题目提交

---
+++ 算法篇/SUYI/2.链表/2.3反转链表.cpp
@@ -0,0 +1,44 @@
+//206. 反转链表
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        ListNode* temp;
+        while (cur != NULL) {
+            temp = cur->next;
+            cur->next = pre;
+            if (pre == head) {
+                pre->next = NULL;
+            }
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+};
