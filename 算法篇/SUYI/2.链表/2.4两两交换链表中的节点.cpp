3dd3caa7fdf23abb7658d94ce48bc839abffa124
SUYI-G <13052209268@163.com>
2021-10-06 20:19:54 +0800

    10.6题目提交

---
+++ 算法篇/SUYI/2.链表/2.4两两交换链表中的节点.cpp
@@ -0,0 +1,43 @@
+//24. 两两交换链表中的节点
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != nullptr && cur->next->next != nullptr) {
+            ListNode* temp = cur->next;
+            ListNode* temp1 = cur->next->next->next;
+            cur->next = cur->next->next;
+            cur->next->next = temp;
+            cur->next->next->next = temp1;
+            cur = cur->next->next;
+        }
+        return dummyHead->next;
+    }
+};
