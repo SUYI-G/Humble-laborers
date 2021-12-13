e1f0086fbf3f3a9f40077b6308e47be9b37bc6e5
Wang Ming <18817622150@163.com>
2021-12-13 14:23:49 +0800

    Create 递归-最小深度.cpp
    12.13 公祭日 勿忘国耻

---
+++ 算法篇/Wang_Ming/二叉树/递归-最小深度.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (root -> left && root -> right) return 1 + min(minDepth(root -> left), minDepth(root -> right));
+        if (root -> left) return 1 + minDepth(root -> left);
+        if (root -> right) return 1 + minDepth(root -> right);
+        return 1; 
+
+    }
+};
